fdacd33faa84c611afe2b31ebf9542e9146374a4
Syed Mahboob Nur <mahboobflorattd@gmail.com>
2022-04-24 21:16:31 +0600

    20220424

---
+++ Demo/CategoryViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Mahboob Nur on 24/4/22.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+    
+    var categoryView = UIView()
+    @IBOutlet weak var scrolParent: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        designCategory()
+        // Do any additional setup after loading the view.
+    }
+
+    func designCategory()
+    {
+        var lblCategoryTitle = UILabel()
+        var viewItem = UIView()
+        scrolParent.contentSize = CGSize(width: scrolParent.frame.size.width, height: scrolParent.frame.size.height*100)
+        var xpos = 0 , ypos = 0
+        var itXpos = 0, itYpos = 0
+        var itemScroll = UIScrollView()
+        for i in 1...100
+        {
+            categoryView = UIView(frame:CGRect(x:CGFloat(xpos), y: CGFloat(ypos), width:self.view.frame.size.width, height:self.view.frame.size.width))
+            categoryView.backgroundColor = UIColor.orange
+            scrolParent.addSubview(categoryView)
+            
+            lblCategoryTitle = UILabel(frame: CGRect(x:5, y: 10, width:200, height:50))
+            lblCategoryTitle.text = "Category:\(i)"
+            categoryView.addSubview(lblCategoryTitle)
+            itemScroll = UIScrollView(frame: CGRect(x:0, y: 50, width:categoryView.frame.size.width, height:categoryView.frame.size.height))
+            categoryView.addSubview(itemScroll)
+            itemScroll.contentSize = CGSize(width: itemScroll.frame.size.width*5, height: itemScroll.frame.size.height)
+            for j in 1...27
+            {
+                viewItem = UIView(frame: CGRect(x:5, y: 50, width:200, height:50))
+                itemScroll.addSubview(viewItem)
+                viewItem.backgroundColor = UIColor.blue
+            
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            ypos = ypos + Int(categoryView.frame.size.height) + 5
+        }
+    }
+
+}
+
